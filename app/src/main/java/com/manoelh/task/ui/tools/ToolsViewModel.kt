9c5484629eeb45ca63b20546c8b79bfbae66821f
Manoel Henrique <henriqe115@gmail.com>
2019-12-24 19:04:54 -0300

    first commit

---
+++ app/src/main/java/com/manoelh/task/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package com.manoelh.task.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
