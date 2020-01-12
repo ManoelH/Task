2d187cff756df7c865cfae85f65da7a176ab6148
Manoel Henrique <henriqe115@gmail.com>
2020-01-12 13:41:36 -0300

    password validation in two editTexts to confirm the password

--- app/src/main/java/com/manoelh/task/views/activity/RegisterActivity.kt
+++ app/src/main/java/com/manoelh/task/views/activity/RegisterActivity.kt
@@ -3,24 +3,34 @@ package com.manoelh.task.views.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
 import com.manoelh.task.R
 import com.manoelh.task.business.UserBusiness
 import com.manoelh.task.entity.UserEntity
 import com.manoelh.task.util.ValidationException
 import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity(), View.OnClickListener {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
 
-    lateinit var user: UserEntity
-    lateinit var userBusiness: UserBusiness
+    private lateinit var user: UserEntity
+    private lateinit var userBusiness: UserBusiness
+    private var thePasswordsAreDifferent = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        buttonRegister.setOnClickListener(this)
+        setListeners()
         userBusiness = UserBusiness(this)
+    }
+
+    private fun setListeners() {
+        buttonRegister.setOnClickListener(this)
+        editTextRewritePassword.addTextChangedListener(this)
+        editTextPassword.addTextChangedListener(this)
     }
 
     override fun onClick(view: View) {
@@ -30,19 +40,39 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun registerUser(){
         try {
-            val name = editTextName.text.toString()
-            val email = editTextEmail.text.toString()
-            val password = editTextPassword.text.toString()
-            user = UserEntity(name = name, email = email, password = password)
-            userBusiness.registerUser(user)
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            if (thePasswordsAreDifferent)
+                Toast.makeText(this, this.getText(R.string.the_passwords_are_not_equals), Toast.LENGTH_SHORT).show()
+            else{
+                val name = editTextName.text.toString()
+                val email = editTextEmail.text.toString()
+                val password = editTextPassword.text.toString()
+                user = UserEntity(name = name, email = email, password = password)
+                userBusiness.registerUser(user)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }catch (ve: ValidationException){
             Toast.makeText(this, ve.message, Toast.LENGTH_LONG).show()
         }catch (e: Exception){
             Toast.makeText(this, getString(R.string.genericError), Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        verifyIfThePasswordAreEquals()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    private fun verifyIfThePasswordAreEquals(){
+        textViewPasswordsAreNotEquals.isVisible =
+            (editTextPassword.text.toString() != editTextRewritePassword.text.toString())
+        thePasswordsAreDifferent = textViewPasswordsAreNotEquals.isVisible
     }
 
 }
